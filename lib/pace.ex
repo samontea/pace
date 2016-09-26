1e9c438a1072bfdc45ddf1f0b387d3802b8ada51
samontea <merciers.merciers@gmail.com>
2016-09-26 15:18:01 -0400

    add pid to logger info

--- lib/pace.ex
+++ lib/pace.ex
@@ -8,7 +8,7 @@ defmodule Pace do
   defp listen(start_time) do
     receive do
       :stop ->
-        Logger.debug "stopped:\n#{(System.monotonic_time(:nanoseconds) - start_time) / 1000000} milliseconds"
+        Logger.debug "#{Kernel.inspect self} stopped:\n#{(System.monotonic_time(:nanoseconds) - start_time) / 1000000} milliseconds"
     end
   end
 end
