738916212fa68dd70476eb205795ffffd09eecce
samontea <merciers.merciers@gmail.com>
2016-10-06 23:48:16 -0400

    added restart, lap functionality

--- lib/pace.ex
+++ lib/pace.ex
@@ -7,6 +7,11 @@ defmodule Pace do
 
   defp listen(start_time) do
     receive do
+      :restart ->
+        listen(System.monotonic_time(:nanoseconds))
+      {:lap, message} ->
+        Logger.debug "#{Kernel.inspect self} #{(System.monotonic_time(:nanoseconds) - start_time) / 1000000} milliseconds:\n#{message} "
+        listen(start_time)
       :stop ->
         Logger.debug "#{Kernel.inspect self} stopped:\n#{(System.monotonic_time(:nanoseconds) - start_time) / 1000000} milliseconds"
     end
